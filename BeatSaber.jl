b390e905e2556e9c509025f3adc01ec9d79a7bcf
Lucien Maloney <hobbesdevelopment@gmail.com>
2019-11-29 15:29:39 -0600

    Progress

--- BeatSaber.jl
+++ BeatSaber.jl
@@ -1,6 +1,7 @@
 module BeatSaber
   using WAV
   using JSON
+  using DelimitedFiles
 
   export mapSong
 
@@ -85,8 +86,84 @@ module BeatSaber
     return notes
   end
 
+  function timesToNotes(noteTimes::Array{<:Number})::Array{Dict}
+    patterns = JSON.parsefile("patterns.json")
+    patternkeys = collect(keys(patterns))
+    previousnote = missing
+    penultimatenote = missing
+    notes = []
+
+    for notetime in noteTimes
+      if !haskey(patterns, previousnote)
+        penultimatenote = missing
+        previousnote = rand(patternkeys)
+      end
+
+      if !haskey(patterns[previousnote], penultimatenote)
+        tempkeys = collect(keys(patterns[previousnote]))
+        penultimatenote = rand(tempkeys)
+      end
+
+      dict = patterns[previousnote][penultimatenote]
+      total = values(dict) |> sum
+      target = rand(1:total)
+      counter = 0
+
+      for k in keys(dict)
+        counter += dict[k]
+        if counter >= target
+          penultimatenote = previousnote
+          previousnote = k
+          for i = 1:4:length(k)
+            push!(notes, createNote(k[i:(i + 3)], notetime))
+          end
+          counter = -10000
+        end
+      end
+    end
+
+    return notes
+  end
+
+  function noteindex(x::Int, y::Int, dir::Int)::Int
+    return x + y * 4 + dir * 12
+  end
+
+  function notedata(note::Int)::Tuple
+    x = (note - 1) % 4
+    y = div((note - 1), 4) % 3
+    direction = div((note - 1), 12)
+
+    return (x, y, direction)
+  end
+
+  function mapSingleHand(noteTimes::Array{<:Number})::Array{Dict}
+    notepatterns = readdlm("notes.csv", Int8)
+    starternote = (rand(1:2), rand(1:2), rand(0:7))
+    nindex = noteindex(starternote...)
+    notes = [createNote(0, starternote[3], noteTimes[1], starternote[1], starternote[2])]
+
+    for i=2:length(noteTimes)
+      timeDiff = noteTimes[i] - noteTimes[i - 1]
+      index = rand(1:96)
+      notefound = false
+      while !notefound
+        index = index % 96 + 1
+        level = notepatterns[nindex, index]
+        if (level == 0 && timeDiff <= 0.05) || (level == 1 && timeDiff <= 0.1) || (level == 2 && timeDiff > 0.1) || (level == 3 && timeDiff > 0.25) || (level == 4 && timeDiff > 0.75)
+          nindex = index
+          newnote = notedata(index)
+          push!(notes, createNote(0, newnote[3], noteTimes[i], newnote[1], newnote[2]))
+          notefound = true
+        end
+      end
+    end
+
+    return notes
+  end
+
   function createMapJSON(noteTimes::Array{T})::String where {T<:Number}
-    notes = mapNotes(noteTimes)
+    notes = timesToNotes(noteTimes)
 
     songData = Dict(
       "_version" => "2.0.0",
@@ -121,7 +198,7 @@ module BeatSaber
     if match(r".wav$"i, filename) == nothing
       run(`ffmpeg -i $filename $wavfile`)
     else
-      cp(songname, wavfile)
+      cp(filename, wavfile)
     end
 
     write("$songname/ExpertPlus.dat", createMap(wavfile))
@@ -132,6 +209,7 @@ module BeatSaber
 
     infostring = String(read("info.dat"))
     write("$songname/info.dat", replace(infostring, "<SongName>" => songname))
+    cp("cover.jpg", "$songname/cover.jpg")
   end
 
 end
